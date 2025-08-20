168f1edd57737dd6f888b9eb7c14e73f85ff88d3
hanishchuger1313 <hanishchuger1313@gmail.com>
2025-08-21 01:43:32 +0530

    Add files via upload

---
+++ ratan-store-fixed/src/index.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Ratan Store 🎉</h1>
+      <p>Your Seasonal & Boys Jewellery Store</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
